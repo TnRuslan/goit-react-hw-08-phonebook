31cd8ab9267bac6bbd7ae101a2425e06a5a716c6
Ruslan Tendora <ruslantendora@gmail.com>
2023-01-24 23:27:55 +0200

    add user's icon

--- src/components/UserMenu/UserMenu.jsx
+++ src/components/UserMenu/UserMenu.jsx
@@ -1,17 +1,19 @@
 import { useSelector } from 'react-redux';
 import { LogoutBtn } from 'components/LogOutBtn/Logout.btn';
 import { selectUserName } from 'redux/selectors';
+import { FaUserCircle } from 'react-icons/fa';
 import css from './UserMenu.module.css';
 
 export const UserMenu = () => {
   const user = useSelector(selectUserName);
   return (
     <div className={css.user_menu}>
-      {/* <img
-        src="https://via.placeholder.com/100x150.png?text=Profile/"
-        alt="user photo"
-      /> */}
-      <p className={css.user_name}>{user}</p>
+      <div className={css.user_wrapper}>
+        <FaUserCircle className={css.user_icon} />
+
+        <p className={css.user_name}>{user}</p>
+      </div>
+
       <LogoutBtn />
     </div>
   );
